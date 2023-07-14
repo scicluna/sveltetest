8226ffca5a3695bc3ce1c9e181d970226f3e3db7
scicluna <88948700+scicluna@users.noreply.github.com>
2023-07-14 15:45:35 -0400

    use:enhance and form actions and animations very cool

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,109 @@
-<script>
-  import { message } from "$lib/message"
+<script lang="ts">
+  import { enhance } from "$app/forms"
+  import { fly, slide } from "svelte/transition"
+
+  export let data
+  export let form
+
+  let creating = false
+  /**
+   * @type {string | any[]}
+   */
+  let deleting: any[] = []
 </script>
 
-<h1>home</h1>
-<p>{message}</p>
+<div class="centered">
+  <h1>todos</h1>
+
+  {#if form?.error}
+    <p class="error">{form.error}</p>
+  {/if}
+
+  <form
+    method="POST"
+    action="?/create"
+    use:enhance={() => {
+      creating = true
+      return async ({ update }) => {
+        await update()
+        creating = false
+      }
+    }}
+  >
+    <label>
+      add a todo:
+      <input
+        disabled={creating}
+        name="description"
+        autocomplete="off"
+        required
+        value={form?.description ?? ""}
+      />
+    </label>
+  </form>
+
+  <ul class="todos">
+    {#each data.todos.filter((todo) => !deleting.includes(todo.id)) as todo}
+      <li in:fly={{ x: -20 }} out:slide>
+        <form
+          method="POST"
+          action="?/delete"
+          use:enhance={() => {
+            deleting = [...deleting, todo.id]
+            return async ({ update }) => {
+              await update()
+              deleting = deleting.filter((id) => id !== todo.id)
+            }
+          }}
+        >
+          <input type="hidden" name="id" value={todo.id} />
+          <span>{todo.description}</span>
+          <button aria-label="Mark as complete" />
+        </form>
+      </li>
+    {/each}
+  </ul>
+
+  {#if creating}
+    <span class="saving">saving...</span>
+  {/if}
+</div>
+
+<style>
+  .centered {
+    max-width: 20em;
+    margin: 0 auto;
+  }
+
+  label {
+    width: 100%;
+  }
+
+  input {
+    color: black;
+    flex: 1;
+  }
+
+  span {
+    flex: 1;
+  }
+
+  button {
+    border: none;
+    background: url(./remove.svg) no-repeat 50% 50%;
+    background-size: 1rem 1rem;
+    cursor: pointer;
+    height: 100%;
+    aspect-ratio: 1;
+    opacity: 0.5;
+    transition: opacity 0.2s;
+  }
+
+  button:hover {
+    opacity: 1;
+  }
+
+  .saving {
+    opacity: 0.5;
+  }
+</style>
